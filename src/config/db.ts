4da051939c16cae434d54d96f8fc61ea8e7986a5
Arpita Baraskar <arpitabaraskar22@gmail.com>
2026-03-02 21:24:55 +0530

    all the conflicts are solved

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,6 @@
 import { Pool } from "pg";
 
-// const requiredEnvVars = ["DB_USER", "DB_HOST", "DB_NAME", "DB_PASSWORD", "DB_PORT"];
-// const missingVars = requiredEnvVars.filter(v => !process.env[v]);
-
-// if (missingVars.length > 0) {
-//   throw new Error(`Missing environment variables: ${missingVars.join(", ")}`);
-// }
-
 export const pool = new Pool({
-    
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
